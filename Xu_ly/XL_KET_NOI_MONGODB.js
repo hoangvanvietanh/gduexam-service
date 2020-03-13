fb0a4cf48efff4eb4bc99b5d94c9c13bf6eb1ba1
Hoàng Văn Việt Anh <37948602+hoangvanvietanh@users.noreply.github.com>
2020-03-13 17:01:08 +0700

    done nè
    done

--- Xu_ly/XL_KET_NOI_MONGODB.js
+++ Xu_ly/XL_KET_NOI_MONGODB.js
@@ -5,8 +5,8 @@ var DbConnection = function() {
     var instance = 0
     async function DbConnect() {
         try {
-            //var url = `mongodb://localhost:27017`
-            var url = `mongodb+srv://vietanh:113114115@cluster0-jfbgb.mongodb.net/test?retryWrites=true&w=majority`
+            var url = `mongodb://125.234.139.153:443`
+            //var url = `mongodb+srv://vietanh:113114115@cluster0-jfbgb.mongodb.net/test?retryWrites=true&w=majority`
             //var url = `mongodb+srv://vietanh:113114115@server-nnbiq.gcp.mongodb.net/test?retryWrites=true&w=majority`
             var _db = await MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
             return _db.db(`gia_dinh_university`)
